fdbdf071eef030deb01b3ff8350a603568014aa4
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-07-24 20:53:20 +0300

    updddd

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-// import pcss from "../contform/form.module.css";
-
-// export const Filter = ({ filter, onFilterChange }) => {
-//   return (
-    
-//     <label className={pcss.clabel} htmlFor="filterInput">
-//       Find contacts by name:
-//       <input
-//         className={pcss.cinput}
-//         id="filterInput"
-//         type="text"
-//         name="filter"
-//         value={filter}
-//         onChange={onFilterChange}
-//       />
-//     </label>
-    
-//   );
-// };
 
 import React from "react";
 import pcss from "../contform/form.module.css";
--- src/components/redux/filterSlice.jsx
+++ src/components/redux/filterSlice.jsx
@@ -1,20 +1,3 @@
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const filterState = '';
-
-// export const filterSlice = createSlice({
-//   name: 'filter',
-//   initialState: filterState,
-//   reducers: {
-//     filterUser(state, action) {
-//       return (state = action.payload);
-//     },
-//   },
-// });
-
-// export const { filterUser } = filterSlice.actions;
-// export const filterReducer = filterSlice.reducer;
-
 
 import { createSlice } from '@reduxjs/toolkit';
 
